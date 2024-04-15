5c8acc8c501981db7f2538f4618bebe33e391f56
Mark Czotter <mczotter@b2international.com>
2024-04-15 21:38:06 +0200

    feat(r4b): add R4B object classes
    Apply the necessary changes as described in steps 5 and 7.

---
+++ src/main/java/org/hl7/fhir/r4b/model/Duration.java
@@ -0,0 +1,41 @@
+package org.hl7.fhir.r4b.model;
+
+import org.hl7.fhir.instance.model.api.ICompositeType;
+
+import ca.uhn.fhir.model.api.annotation.DatatypeDef;
+
+/**
+ * Base StructureDefinition for Duration Type: A length of time.
+ */
+@DatatypeDef(name = "Duration")
+public class Duration extends Quantity implements ICompositeType {
+
+  private static final long serialVersionUID = 0L;
+
+  /**
+   * Constructor
+   */
+  public Duration() {
+    super();
+  }
+
+  public String fhirType() {
+    return "Duration";
+
+  }
+
+  public Duration copy() {
+    Duration dst = new Duration();
+    copyValues(dst);
+    return dst;
+  }
+
+  public void copyValues(Duration dst) {
+    super.copyValues(dst);
+  }
+
+  protected Duration typedCopy() {
+    return copy();
+  }
+
+}
