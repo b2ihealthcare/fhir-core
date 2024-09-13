454cff4a8cc750700d497a398c1902f6216376a7
Mark Czotter <mczotter@b2international.com>
2024-09-13 10:59:07 +0200

    refactor: remove all copy-pasted fhir.core classes from the repo

--- src/main/java/org/hl7/fhir/convertors/conv43_50/datatypes43_50/special43_50/xhtml43_50.java
+++
@@ -1,9 +0,0 @@
-package org.hl7.fhir.convertors.conv43_50.datatypes43_50.special43_50;
-
-import org.hl7.fhir.exceptions.FHIRException;
-
-public class xhtml43_50 {
-  public static org.hl7.fhir.utilities.xhtml.XhtmlNode convertXhtml(org.hl7.fhir.utilities.xhtml.XhtmlNode src) throws FHIRException {
-    return src;
-  }
-}
