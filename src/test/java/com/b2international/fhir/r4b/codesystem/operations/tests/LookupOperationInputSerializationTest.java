9702d29b1fdc6b33a23ff9026aae196f21f6f6ec
Adam Filep <afilep@b2international.com>
2024-09-16 09:40:16 +0200

    test(fhir): add basic test cases for codesystem lookup parsing in r4b

---
+++ src/test/java/com/b2international/fhir/r4b/codesystem/operations/tests/LookupOperationInputSerializationTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2024 B2i Healthcare, https://b2ihealthcare.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.fhir.r4b.codesystem.operations.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.hl7.fhir.r4b.formats.JsonParser;
+import org.hl7.fhir.r4b.model.Parameters;
+import org.hl7.fhir.r4b.model.Resource;
+import org.junit.Test;
+
+/**
+ * @since 0.1
+ */
+public class LookupOperationInputSerializationTest {
+	
+	private final JsonParser parser = new JsonParser();
+	
+	@Test
+	public void code_and_system_param() throws Exception {
+		
+		String json = """
+		{
+			"resourceType": "Parameters",
+			"parameter" : [
+				{
+					"name": "code",
+					"valueCode": "testCode" 
+				},
+				{
+					"name": "system",
+					"valueCode": "testSystem"
+				}	
+			]
+		}""";
+		
+		Resource resource = parser.parse(json);
+		
+		assertThat(resource).isInstanceOf(Parameters.class);
+	}
+	
+	@Test
+	public void all_params() throws Exception {
+		String json = """
+		{
+			"resourceType": "Parameters",
+			"parameter" : [
+				{
+					"name": "code",
+					"valueCode": "testCode" 
+				},
+				{
+					"name": "system",
+					"valueCode": "testSystem"
+				},
+				{
+					"name": "version",
+					"valueString": "testVersion"
+				},
+				{
+					"name": "coding",
+					"valueCoding": {
+							"system": "testValueCodingSystem",
+							"code": "testCodingValueCode",
+							"display": "testDisplay"
+					}
+				},
+				{
+					"name": "date",
+					"valueDate": "2024"
+				},
+				{
+					"name": "displayLanguage",
+					"valueCode": "en"
+				},
+				{
+					"name": "property",
+					"valueCode": "testCode"
+				},
+				{
+					"name": "useSupplement",
+					"valueCanonical": "testUri"
+				}
+			]
+		}""";
+		
+		Resource resource = parser.parse(json);
+		
+		assertThat(resource).isInstanceOf(Parameters.class);
+		
+	}
+	
+}
---
+++ src/test/java/com/b2international/fhir/r4b/codesystem/operations/tests/LookupOperationResultSerializationTest.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2024 B2i Healthcare, https://b2ihealthcare.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.b2international.fhir.r4b.codesystem.operations.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.hl7.fhir.r4b.formats.JsonParser;
+import org.hl7.fhir.r4b.model.Parameters;
+import org.hl7.fhir.r4b.model.Resource;
+import org.junit.Test;
+
+
+/**
+ * @since 0.1
+ */
+public class LookupOperationResultSerializationTest {
+	
+	private final JsonParser parser = new JsonParser();
+	
+	@Test
+	public void basic_parameters() throws Exception {
+		String json = """
+		{
+			"resourceType": "Parameters",
+			"parameter" : [
+				{
+					"name": "code",
+					"valueString": "testCode" 
+				},
+				{
+					"name": "system",
+					"valueString": "testSystem"
+				},
+				{
+					"name": "display",
+					"valueString": "testDisplay"
+				},
+				{
+					"name": "definition",
+					"valueString": "Test definition"
+				}	
+			]
+		}""";
+		
+		Resource resource = parser.parse(json);
+		
+		assertThat(resource).isInstanceOf(Parameters.class);
+	}
+	
+	@Test
+	public void designation_without_property() throws Exception {
+		
+		String json = """
+		{
+			"resourceType": "Parameters",
+			"parameter" : [
+				{
+					"name": "code",
+					"valueString": "testCode" 
+				},
+				{
+					"name": "system",
+					"valueString": "testSystem"
+				},
+				{
+					"name": "display",
+					"valueString": "testDisplay"
+				},
+				{
+					"name": "definition",
+					"valueString": "Test definition"
+				},
+				{
+					"name": "designation",
+					"part": [
+						{
+							"name": "language",
+							"valueCode": "en"
+						},
+						{
+							"name": "use",
+							"valueCoding": {
+								"system": "testCodingSystem",
+								"code": "testCodingCode",
+								"display": "Display test"
+							}
+						},
+						{
+							"name": "additionalUse",
+							"valueCoding": {
+								"system": "addTestCodingSystem2",
+								"code": "addTestCodingCode",
+								"display": "Additional Display test"
+							}
+						},
+						{
+							"name": "value",
+							"valueString": "testValue"
+						}
+					]
+				}
+			]
+		}""";
+		
+		Resource resource = parser.parse(json);
+		
+		assertThat(resource).isInstanceOf(Parameters.class);
+	}
+	
+	@Test
+	public void property_without_designation() throws Exception {
+		String json = """
+		{
+			"resourceType": "Parameters",
+			"parameter" : [
+				{
+					"name": "code",
+					"valueString": "testCode" 
+				},
+				{
+					"name": "system",
+					"valueString": "testSystem"
+				},
+				{
+					"name": "display",
+					"valueString": "testDisplay"
+				},
+				{
+					"name": "definition",
+					"valueString": "Test definition"
+				},
+				{
+					"name": "property",
+					"part": [
+						{
+							"name": "code",
+							"valueCode": "testPropertyCode"
+						},
+						{
+							"name": "value",
+							"valueCoding": {
+								"system": "testCodingSystem",
+								"code": "testCodingCode",
+								"display": "Display test"
+							}
+						},
+						{
+							"name": "description",
+							"valueString": "Test description"
+						},
+						{
+							"name": "source",
+							"valueCanonical": "testUri"
+						},
+						{
+							"name": "subproperty",
+							"part": [
+								{
+									"name": "code",
+									"valueCode": "testSubPropertyCode"
+								},
+								{
+									"name": "value",
+									"valueCoding": {
+										"system": "testSubCodingSystem",
+										"code": "testSubCodingCode",
+										"display": "Sub Display test"
+									}
+								},
+								{
+									"name": "description",
+									"valueString": "Test subdescription"
+								},
+								{
+									"name": "source",
+									"valueCanonical": "testSubUri"
+								}
+							]
+						}
+					]
+				}
+			]
+		}""";
+				
+		Resource resource = parser.parse(json);
+		
+		assertThat(resource).isInstanceOf(Parameters.class);
+	}
+}
